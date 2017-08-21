ed58c1fc9e495a5b8854ffd74feb753e77edec52
ritiek <ritiekmalhotra123@gmail.com>
2017-08-21 23:08:22 +0530

    I have no idea what's going on

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,19 @@ use std::fs::File;
 use std::io::prelude::*;
 
 
-fn factorial(value: u32) -> u32 {
+fn factorial(value: usize) -> usize {
     if value == 0 { 1 }
     else { value * factorial(value-1) }
 }
 
 
-fn generate(n: u32, vchars: &Vec<char>, olength: u32, tlength: u32) -> String {
+fn generate(n: usize, vchars: &Vec<char>, olength: usize, tlength: usize) -> String {
     let mut seed = String::new();
-    for alphabet in n.to_string().chars() {
-        seed += &alphabet.to_string();
+    for i in 0..olength {
+        seed += &vchars[i].to_string();
+    }
+    while seed.len() < olength {
+        seed += &vchars[0].to_string();
     }
     seed
 }
@@ -25,10 +28,10 @@ fn main() {
     let schars: &str = "123abc";
     let mut vchars: Vec<char> = schars.chars().collect();
     let textfile = "text.txt";
-    let olength = 5;
+    let olength: usize = 5;
     let mut file = File::create(textfile);
 
-    let tlength = vchars.len() as u32;
+    let tlength = vchars.len();
     vchars.sort();
     let tcombinations = factorial(tlength)/factorial(tlength-olength);
     let mut textfile = OpenOptions::new()
