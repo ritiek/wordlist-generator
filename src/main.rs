2c8afb9b1642116d486894a848d76e355bbbd1da
ritiek <ritiekmalhotra123@gmail.com>
2017-08-27 21:06:39 +0530

    Add command-line usage

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,21 @@
-//extern crate clap;
 extern crate itertools;
 extern crate permutohedron;
+#[macro_use(value_t)]
+extern crate clap;
 
-//use clap::{Arg, App};
 use itertools::Itertools;
 use permutohedron::heap_recursive;
-use std::fs::OpenOptions;
-use std::fs::File;
-use std::io::prelude::*;
+use clap::{Arg, App};
 
 
-fn make_combinations(vchars: Vec<char>, olength: usize) -> Vec<Vec<char>> {
-    let preset = (0..vchars.len()).combinations(olength);
+fn make_combinations(charlist: Vec<char>, olength: usize) -> Vec<Vec<char>> {
+    let preset = (0..charlist.len()).combinations(olength);
     let mut combinations = Vec::new();
 
-    for comb in preset {
+    for precomb in preset {
         let mut combination = Vec::new();
-        for item in comb {
-            combination.push(vchars[item]);
+        for preitem in precomb {
+            combination.push(charlist[preitem]);
         }
         combinations.push(combination);
     }
@@ -38,14 +36,40 @@ fn make_permutations(combinations: Vec<Vec<char>>) -> Vec<Vec<char>> {
 
 
 fn main() {
-    let schars: &str = "123abc";
-    let mut vchars: Vec<char> = schars.chars().collect();
-    let olength: usize = 5;
+	let arguments = App::new("wordlist-generator")
+                    .version("0.1.0")
+                    .author("Ritiek Malhotra <ritiekmalhotra123@gmail.com>")
+                    .about("A handy wordlist generator")
 
-    vchars.sort();
-    let combinations = make_combinations(vchars, olength);
+                    .arg(Arg::with_name("charlist")
+                    .index(1)
+                    .short("c")
+                    .long("character-list")
+                    .value_name("CHARS")
+                    .takes_value(true)
+                    .required(true)
+                    .help("Characters to generate permutations from"))
+
+                    .arg(Arg::with_name("olength")
+                    .short("o")
+                    .long("output-length")
+                    .value_name("LENGTH")
+                    .takes_value(true)
+                    .help("Output length of the generated permutations"))
+
+                    .get_matches();
+
+    let charlist: &str = arguments.value_of("charlist").unwrap();
+    let olength: usize = value_t!(arguments, "olength", usize).unwrap_or(charlist.len());
+
+    let mut charlist: Vec<char> = charlist.chars().collect();
+    charlist.sort();
+
+    let combinations = make_combinations(charlist, olength);
     let permutations = make_permutations(combinations);
-    for perm in permutations {
-        println!("{:?}", perm);
+
+    for permutation in permutations {
+        let stringform: String = permutation.into_iter().collect();
+        println!("{}", stringform);
     }
 }
