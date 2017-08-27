7300b84a9feb9e5c4cf486b185763e7502bdcb1a
ritiek <ritiekmalhotra123@gmail.com>
2017-08-27 19:21:00 +0530

    Some progress at last

--- src/main.rs
+++ src/main.rs
@@ -8,26 +8,44 @@ use permutohedron::heap_recursive;
 use std::fs::OpenOptions;
 use std::fs::File;
 use std::io::prelude::*;
-use collections::slice::Permutations;
+
+
+fn make_combinations(vchars: Vec<char>, olength: usize) -> Vec<Vec<char>> {
+    let preset = (0..vchars.len()).combinations(olength);
+    let mut combinations = Vec::new();
+
+    for comb in preset {
+        let mut combination = Vec::new();
+        for item in comb {
+            combination.push(vchars[item]);
+        }
+        combinations.push(combination);
+    }
+
+    combinations
+}
+
+
+fn make_permutations(combinations: Vec<Vec<char>>) -> Vec<Vec<char>> {
+    let mut permutations = Vec::new();
+    for mut combination in combinations {
+        heap_recursive(&mut combination, |permutation| {
+            permutations.push(permutation.to_vec())
+        });
+    }
+    permutations
+}
 
 
 fn main() {
-    let schars = String::from("123abc");
-    //let schars: &str = "123abc";
-    //let mut vchars: Vec<char> = schars.chars().collect();
-    let textfile = "text.txt";
-    let olength: u32 = 5;
+    let schars: &str = "123abc";
+    let mut vchars: Vec<char> = schars.chars().collect();
+    let olength: usize = 5;
 
-    let mut file = File::create(textfile);
-
-    //vchars.sort();
-    //let it = (1..5).combinations(3);
-    let it = schars.combinations(2);
-    let mut textfile = OpenOptions::new()
-                .write(true)
-                .append(true)
-                .open(textfile)
-                .unwrap();
-
-    //writeln!(textfile, seed);
+    vchars.sort();
+    let combinations = make_combinations(vchars, olength);
+    let permutations = make_permutations(combinations);
+    for perm in permutations {
+        println!("{:?}", perm);
+    }
 }
